d50301f68bf32dbcb0d75276ea170d48b40c160e
Jakub <60443955+jakubkobus@users.noreply.github.com>
2025-04-11 17:24:45 +0200

    feat: get queries

--- app/controllers/search_queries_controller.ts
+++ app/controllers/search_queries_controller.ts
@@ -1,12 +1,22 @@
-import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from "@adonisjs/core/http";
 
-import SearchQuery from '#models/search_query'
-import { createSearchQueryValidator } from '#validators/search_query'
+import SearchQuery from "#models/search_query";
+import { createSearchQueryValidator } from "#validators/search_query";
 
 export default class SearchQueriesController {
   async store({ request, response }: HttpContext) {
-    const data = await request.validateUsing(createSearchQueryValidator)
-    const searchQuery = await SearchQuery.create(data)
-    return response.created(searchQuery)
+    const data = await request.validateUsing(createSearchQueryValidator);
+    const searchQuery = await SearchQuery.create(data);
+    return response.created(searchQuery);
+  }
+
+  async index({ response }: HttpContext) {
+    const searchQueries = await SearchQuery.query();
+    return response.json(searchQueries);
+  }
+
+  async show({ params, response }: HttpContext) {
+    const searchQuery = await SearchQuery.findOrFail(params.id);
+    return response.json(searchQuery);
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -6,10 +6,14 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
+import router from "@adonisjs/core/services/router";
 
-import router from '@adonisjs/core/services/router'
-const SearchQueriesController = () => import("#controllers/search_queries_controller")
+const SearchQueriesController = () =>
+  import("#controllers/search_queries_controller");
 
-router.get('/', async () => 'It works!')
+router.get("/", async () => "It works!");
 
-router.post('api/v1/search-queries', [SearchQueriesController, 'store'])
+router.post("api/v1/search-queries", [SearchQueriesController, "store"]);
+
+router.get("api/v1/search-queries", [SearchQueriesController, "index"]);
+router.get("api/v1/search-queries/:id", [SearchQueriesController, "show"]);
