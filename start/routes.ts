e5cf3114069d8e873ea5868196f94df2bcc380c7
Jakub <60443955+jakubkobus@users.noreply.github.com>
2025-04-12 15:35:48 +0200

    fix: router resource

--- start/routes.ts
+++ start/routes.ts
@@ -11,9 +11,4 @@ import router from "@adonisjs/core/services/router";
 const SearchQueriesController = () =>
   import("#controllers/search_queries_controller");
 
-router.get("/", async () => "It works!");
-
-router.post("api/v1/search-queries", [SearchQueriesController, "store"]);
-
-router.get("api/v1/search-queries", [SearchQueriesController, "index"]);
-router.get("api/v1/search-queries/:id", [SearchQueriesController, "show"]);
+router.resource("api/v1/search-queries", SearchQueriesController);
