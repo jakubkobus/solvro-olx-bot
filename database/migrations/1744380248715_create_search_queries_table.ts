2cd478627dae331e731d7aff07931256380cb012
Jakub <60443955+jakubkobus@users.noreply.github.com>
2025-04-12 03:01:30 +0200

    feat: migration adjustment

--- database/migrations/1744380248715_create_search_queries_table.ts
+++ database/migrations/1744380248715_create_search_queries_table.ts
@@ -1,23 +1,27 @@
-import { BaseSchema } from '@adonisjs/lucid/schema'
+import { BaseSchema } from "@adonisjs/lucid/schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'search_queries'
+  protected tableName = "search_queries";
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.increments("id");
 
-      table.string('name', 255)
-      table.float('price_min')
-      table.float('price_max')
-      table.integer('location_id')
+      table.integer("category_id");
+      table.float("price_min");
+      table.float("price_max");
+      table.integer("location_id");
+      table.integer("disctrict_id");
+      table.integer("region_id");
+      table.integer("pagination_limit");
 
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
-    })
+      table.timestamp("refreshed_at");
+      table.timestamp("created_at");
+      table.timestamp("updated_at");
+    });
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }
