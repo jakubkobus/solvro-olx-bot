a06cc7181fc4888e6d458e9ed0f8bdd4852d33fd
Jakub <60443955+jakubkobus@users.noreply.github.com>
2025-04-12 02:49:15 +0200

    feat: delete search queries

--- start/routes.ts
+++ start/routes.ts
@@ -11,11 +11,18 @@ import router from "@adonisjs/core/services/router";
 const SearchQueriesController = () =>
   import("#controllers/search_queries_controller");
 
-router.get("/", async () => "It works!");
-
+// #1
 router.post("api/v1/search-queries", [SearchQueriesController, "store"]);
 
+// #2
 router.get("api/v1/search-queries", [SearchQueriesController, "index"]);
 router.get("api/v1/search-queries/:id", [SearchQueriesController, "show"]);
 
+// #3
 router.put("api/v1/search-queries/:id", [SearchQueriesController, "update"]);
+
+// #4
+router.delete("api/v1/search-queries/:id", [
+  SearchQueriesController,
+  "destroy",
+]);
